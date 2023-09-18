91af14043cdccdab2718e2872db7ee30cc167663
Katie Snyder <katie.snyder@wegmans.com>
2023-09-18 14:15:20 -0500

    add tests

--- tests/testthat/test-lib-summary.R
+++ tests/testthat/test-lib-summary.R
@@ -1,3 +1,13 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("lib_summary returns expected results", {
+  res = lib_summary()
+  expect_s3_class(res, "data.frame") #expect the result of the function to be a data frame
+  expect_equal(ncol(res), 2) #expect two columns
+  expect_equal(names(res), c("Library", "n_packages"))
+  expect_type(res$Library, "character")
+  expect_type(res$n_packages, "integer")
+}
+)
+
+test_that("lib summary fails appropriately", {
+  expect_error(lib_summary("foo"), "unused argument")
 })
